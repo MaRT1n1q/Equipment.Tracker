90f638b6ee8ad0b9d55b5f56bf54879a06a687c3
Vlad <koltakin.2017@gmail.com>
2025-10-25 19:52:06 +0300

    update preloader

--- electron/preload.js
+++ electron/preload.js
@@ -3,6 +3,10 @@ const { contextBridge, ipcRenderer } = require('electron')
 contextBridge.exposeInMainWorld('electronAPI', {
   getRequests: () => ipcRenderer.invoke('get-requests'),
   createRequest: (data) => ipcRenderer.invoke('create-request', data),
+  updateRequest: (id, data) => ipcRenderer.invoke('update-request', id, data),
   updateIssued: (id, is_issued) => ipcRenderer.invoke('update-issued', id, is_issued),
-  deleteRequest: (id) => ipcRenderer.invoke('delete-request', id)
+  deleteRequest: (id) => ipcRenderer.invoke('delete-request', id),
+  restoreRequest: (request) => ipcRenderer.invoke('restore-request', request),
+  createBackup: () => ipcRenderer.invoke('create-backup'),
+  restoreBackup: () => ipcRenderer.invoke('restore-backup')
 })
